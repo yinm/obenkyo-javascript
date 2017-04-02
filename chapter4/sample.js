990741c2d21d0c3d985b2b32677d9698ad2167b0
yinm <yinm.cminor9@gmail.com>
2017-04-02 17:23:43 +0900

    4.3 done

---
+++ chapter4/sample.js
@@ -0,0 +1,17 @@
+var scope = 'Global Variable';
+
+function checkScope() {
+  var scope = 'Local Variable';
+
+  var f_lit = function() { return scope; }
+  console.log(f_lit());
+
+  var f_con = new Function('return scope;');
+  console.log(f_con());
+}
+
+function litCheckScope() {
+  return scope;
+}
+
+console.log(litCheckScope());
