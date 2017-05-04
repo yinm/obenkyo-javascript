e4b5ee3b5b0c4eca41ce8decbcc70e8635a5dcd0
yinm <yinm.cminor9@gmail.com>
2017-05-04 23:31:32 +0900

    querySelectorAll

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var list = document.getElementsByClassName('my');
+  var list = document.querySelectorAll('#list .hoge');
 
   for (var i = 0, length = list.length; i < length; i++) {
     console.log(list.item(i).href);
