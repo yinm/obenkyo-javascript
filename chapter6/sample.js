0f4535747d3310ea6d4ebebc9560e383c796846f
yinm <yinm.cminor9@gmail.com>
2017-05-04 23:37:10 +0900

    querySelector

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,7 +1,4 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var list = document.querySelectorAll('#list .hoge');
-
-  for (var i = 0, length = list.length; i < length; i++) {
-    console.log(list.item(i).href);
-  }
+  var element = document.querySelector('#list .wai');
+  console.log(element);
 });
