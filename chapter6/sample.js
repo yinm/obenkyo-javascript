0776982e9959481b0680521eb2aaa01d1c521b1a
yinm <yinm.cminor9@gmail.com>
2017-05-05 08:45:21 +0900

    childNodesプロパティ

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,4 +1,12 @@
 window.addEventListener('DOMContentLoaded', function() {
-  var element = document.querySelector('#list .wai');
-  console.log(element);
+  var select = document.getElementById('food');
+  var options = select.childNodes;
+
+  for (var i = 0, length = options.length; i < length; i++) {
+    var option = options.item(i);
+
+    if (option.nodeType === 1) {
+      console.log(option.value);
+    }
+  }
 });
