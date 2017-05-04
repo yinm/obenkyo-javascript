0f48647e966c38285f14ee98a822eea9bcaeb39a
yinm <yinm.cminor9@gmail.com>
2017-05-04 22:53:54 +0900

    getElementById

---
+++ chapter6/sample.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', function() {
+  var current = new Date();
+  var result = document.getElementById('result');
+  result.textContent = current.toLocaleString();
+});
