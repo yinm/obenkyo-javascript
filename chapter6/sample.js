16378a6a12fde8ea7df42299328998dbe102d2ae
yinm <yinm.cminor9@gmail.com>
2017-05-05 10:41:53 +0900

    textContent / innerHTML の取得

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,13 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var resultText = document.getElementById('result_text');
-  var resultHtml = document.getElementById('result_html');
-
-  console.log(resultText);
-  console.log(resultHtml);
-
-  // 取得時の状態を見るため、設定を遅延させる
-  setTimeout(function() {
-    resultText.textContent = '<a href="http://www.example.com">example.com</a>';
-    resultHtml.innerHTML = '<a href="http://www.example.com">example.com</a>';
-  }, 5000);
+  var list = document.getElementById('list');
+  console.log(list.innerHTML);
+  console.log(list.textContent);
 }, false);
