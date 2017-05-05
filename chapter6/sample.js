48bf33404118d278b42a70d48cc2b2f520535451
yinm <yinm.cminor9@gmail.com>
2017-05-05 10:16:24 +0900

    attributesプロパティ

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,5 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('button').addEventListener('click', function() {
-    window.alert('ボタンがクリックされました');
-  }, false);
+  var logo = document.getElementById('logo');
+  var attrs = logo.attributes;
+
+  var title = document.createAttribute('title');
+  title.value = 'ロゴ画像';
+  attrs.setNamedItem(title);
+
+  attrs.removeNamedItem('alt');
+
+  for (var i = 0, length = attrs.length; i < length; i++) {
+    var attr = attrs.item(i);
+    console.log(attr.name + ':' + attr.value);
+  }
 }, false);
