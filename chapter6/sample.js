cf0fa21cc30a6cc9dd4ca2d7e9041be6abdbadc9
yinm <yinm.cminor9@gmail.com>
2017-05-06 09:59:31 +0900

    radioボタンの取得

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('button').addEventListener('click', function() {
-    var toggle = document.getElementById('toggle');
+  var getRadioValue = function(name) {
+    var result = '';
+    var elements = document.getElementsByName(name);
 
-    if (toggle.checked) {
-      console.log(toggle.value);
-    } else {
-      console.log('チェックされていません');
+    for (var i = 0, length = elements.length; i < length; i++) {
+      var element = elements.item(i);
+
+      if (element.checked) {
+        result = element.value;
+        break;
+      }
     }
+
+    return result;
+  };
+
+  document.getElementById('button').addEventListener('click', function() {
+    window.alert(getRadioValue('food'));
   }, false);
 }, false);
