1fdf1bf004975e1322d370fd6830e20843ffae16
yinm <yinm.cminor9@gmail.com>
2017-05-06 09:41:18 +0900

    checkboxのチェック状態を取得する

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,7 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('button').addEventListener('click', function() {
-    var name = document.getElementById('name');
-    console.log(name.value);
-    name.value = '初期化するさん';
+    var result = [];
+    var foods = document.getElementsByName('food');
+
+    for (var i = 0, length = foods.length; i < length; i++) {
+      var food = foods.item(i);
+      if (food.checked) {
+        result.push(food.value);
+      }
+    }
+    window.alert(result.toString());
   }, false);
 }, false);
