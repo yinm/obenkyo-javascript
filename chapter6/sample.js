3ddaab243319c688b3b3888ba070564c16300151
yinm <yinm.cminor9@gmail.com>
2017-05-06 10:44:25 +0900

    複数選択の選択ボックスに、初期値をセット

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var getSelectValue = function(name) {
-    var result = [];
-    var options = document.getElementById(name).options;
+  var setListValue = function(name, value) {
+    var options = document.getElementById(name);
 
     for (var i = 0, length = options.length; i < length; i++) {
       var option = options.item(i);
 
-      if (option.selected) {
-        result.push(option.value);
+      if (value.indexOf(option.value) > -1) {
+        option.selected = true;
       }
     }
-    return result;
   };
 
-  document.getElementById('button').addEventListener('click', function() {
-    window.alert(getSelectValue('food'));
-  }, false);
+  setListValue('food', ['餃子', '焼肉']);
 }, false);
