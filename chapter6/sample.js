02f6f7595858f21dd031e98b5b0aa325788c053f
yinm <yinm.cminor9@gmail.com>
2017-05-06 10:10:53 +0900

    radioボタンに初期値をセット

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,21 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var getRadioValue = function(name) {
-    var result = '';
+  var setRadioValue = function(name, value) {
     var elements = document.getElementsByName(name);
 
     for (var i = 0, length = elements.length; i < length; i++) {
       var element = elements.item(i);
 
-      if (element.checked) {
-        result = element.value;
+      if (element.value === value) {
+        element.checked = true;
         break;
       }
     }
-
-    return result;
   };
 
-  document.getElementById('button').addEventListener('click', function() {
-    window.alert(getRadioValue('food'));
-  }, false);
+  setRadioValue('food', '餃子');
 }, false);
