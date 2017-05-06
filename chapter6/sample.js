a11a466f919dc3bc3ffd36b9d548e71f10a2dbab
yinm <yinm.cminor9@gmail.com>
2017-05-06 09:33:00 +0900

    値を初期化する

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -2,5 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('button').addEventListener('click', function() {
     var name = document.getElementById('name');
     console.log(name.value);
+    name.value = '初期化するさん';
   }, false);
 }, false);
