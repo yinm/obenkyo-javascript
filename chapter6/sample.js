3494d5a8eaadbf2b3543d2a3e8a0899090dbdd37
yinm <yinm.cminor9@gmail.com>
2017-05-07 09:27:44 +0900

    DocumentFragmentを使ったパフォーマンス改善

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   var books = [
     {title: '独習PHP', price: 3200},
-    {title: 'Java poke', price: 2680},
+    {title: 'Java Pocket', price: 2680},
     {title: 'create app', price: 2000}
   ];
 
   var list = document.getElementById('list');
+  var fragment = document.createDocumentFragment();
 
   for (var i = 0, length = books.length; i < length; i++) {
     var book = books[i];
     var li = document.createElement('li');
     var text = document.createTextNode(book.title + ' : ' + book.price + '円');
-
     li.appendChild(text);
-    list.appendChild(li);
+    fragment.appendChild(li);
   }
+
+  document.getElementById('list').appendChild(fragment);
 }, false);
