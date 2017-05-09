137ace6fd6cb22df860bb8f5ad899287cc5a26b0
yinm <yinm@pepabo.com>
2017-05-09 09:40:45 +0900

    HTMLCollectionは動的なオブジェクトであるため、イテレーションの終了条件に使うときは注意する
    終了条件の所で、lengthプロパティを取得するように書くと、イテレーションの度に数値が増えて、無限ループになる。

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  var second = document.getElementById('second');
   var li = document.getElementsByTagName('li');
-  console.log('変更前：' + li.length);
 
-  var ul = document.getElementById('list');
-  ul.appendChild(document.createElement('li'));
-  console.log('変更後：' + li.length);
+  for (var i = 0, length = li.length; i < length; i++) {
+    var item = li.item(i);
+    var new_li = document.createElement('li');
+    var new_text = document.createTextNode(item.textContent);
+    new_li.appendChild(new_text);
+    second.appendChild(new_li);
+  }
 }, false);
