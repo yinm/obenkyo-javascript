32944150fdb12b762fb1dc78b7414a3d0c3fe1c9
yinm <yinm@pepabo.com>
2017-05-09 09:32:35 +0900

    HTMLCollectionオブジェクトに、変更がリアルタイムで反映されることを確認

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,29 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var list = document.getElementById('list');
-  var picture = document.getElementById('picture');
-  var del = document.getElementById('delete');
+  var li = document.getElementsByTagName('li');
+  console.log('変更前：' + li.length);
 
-  list.addEventListener('click', function(e) {
-    var isbn = e.target.getAttribute('data-isbn');
-
-    if (isbn) {
-      var img = document.createElement('img');
-      img.src = 'http://www.wings.msn.to/books/' + isbn + '/' + isbn + '.jpg';
-      img.alt = e.innerHTML;
-      img.height = 150;
-      img.width = 108;
-
-      if (picture.getElementsByTagName('img').length > 0) {
-        picture.replaceChild(img, picture.lastChild);
-      } else {
-        del.disabled = false;
-        picture.appendChild(img);
-      }
-    }
-  }, false);
-
-  del.addEventListener('click', function() {
-    picture.removeChild(picture.lastChild);
-    del.disabled = true;
-  }, false);
+  var ul = document.getElementById('list');
+  ul.appendChild(document.createElement('li'));
+  console.log('変更後：' + li.length);
 }, false);
