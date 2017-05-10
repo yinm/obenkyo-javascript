0c4b271a1bac40a4215942dba0f1a0af37eb1462
yinm <yinm@pepabo.com>
2017-05-10 09:55:51 +0900

    class名のトグル

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var element = document.getElementById('elem');
 
-  element.addEventListener('mouseover', function() {
-    this.className = 'highlight';
-  }, false);
-
-  element.addEventListener('mouseout', function() {
-    this.className = '';
+  element.addEventListener('click', function() {
+    this.className = (this.className === 'highlight' ? '' : 'highlight');
   }, false);
 }, false);
