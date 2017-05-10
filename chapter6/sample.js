f1a52d6a36be8b9348b881607b1ff26c40e37e97
yinm <yinm@pepabo.com>
2017-05-10 13:51:43 +0900

    複数クラスのJS制御

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -2,6 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var element = document.getElementById('elem');
 
   element.addEventListener('click', function() {
-    this.className = (this.className === 'highlight' ? '' : 'highlight');
+    var classes = this.className.split(' ');
+
+    var index = classes.indexOf('highlight');
+    if (index === -1) {
+      classes.push('highlight');
+    } else {
+      classes.splice(index, 1);
+    }
+
+    this.className = classes.join(' ');
   }, false);
 }, false);
