c7090f0971fe0c16f708f2dd12f80f0b56ea6cb2
yinm <yinm@pepabo.com>
2017-05-10 13:55:20 +0900

    classListプロパティを使って、シンプルにする

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -2,15 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var element = document.getElementById('elem');
 
   element.addEventListener('click', function() {
-    var classes = this.className.split(' ');
-
-    var index = classes.indexOf('highlight');
-    if (index === -1) {
-      classes.push('highlight');
-    } else {
-      classes.splice(index, 1);
-    }
-
-    this.className = classes.join(' ');
+    this.classList.toggle('highlight');
   }, false);
 }, false);
