15108d22c5adcf7bb518de375faa133933ae39d0
yinm <yinm@pepabo.com>
2017-05-12 09:37:11 +0900

    キャプチャフェーズのイベント伝播をキャンセルする

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('inner').addEventListener('click', function(e) {
     window.alert('#innerリスナーが発生しました');
-    // 親へのバブリングをキャンセル
-    e.stopPropagation();
-  }, false);
+  }, true);
 
   document.getElementById('inner').addEventListener('click', function(e) {
     window.alert('#innerリスナー2が発生しました');
-  }, false);
+  }, true);
 
   document.getElementById('outer').addEventListener('click', function(e) {
     window.alert('#outerリスナーが発生しました');
-  }, false);
+    // 子要素へのイベント伝播をキャンセル
+    e.stopPropagation();
+  }, true);
 }, false);
