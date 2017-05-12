8346de37c925349a2c3be49fcab27dcf5dd0ce7c
yinm <yinm@pepabo.com>
2017-05-12 09:42:00 +0900

    event.stopImmediatePropagationを使って、イベントの即時キャンセル

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('inner').addEventListener('click', function(e) {
     window.alert('#innerリスナーが発生しました');
-  }, true);
+    e.stopImmediatePropagation();
+  }, false);
 
   document.getElementById('inner').addEventListener('click', function(e) {
     window.alert('#innerリスナー2が発生しました');
-  }, true);
+  }, false);
 
   document.getElementById('outer').addEventListener('click', function(e) {
     window.alert('#outerリスナーが発生しました');
-    // 子要素へのイベント伝播をキャンセル
-    e.stopPropagation();
-  }, true);
+  }, false);
 }, false);
