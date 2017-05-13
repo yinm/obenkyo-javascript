7a54b185865732916da284baa05c0fbfc57258a1
yinm <yinm.cminor9@gmail.com>
2017-05-13 12:30:31 +0900

    handleEventメソッドを使って、EventListenerオブジェクトを指定する

--- chapter6/sample.js
+++ chapter6/sample.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
   var data = {
     title: 'Java Pocket',
     price: 2680,
-    show: function() {
+    handleEvent: function() {
       console.log(this.title + '/' + this.price + '円');
     }
   };
 
-  document.getElementById('button').addEventListener('click', data.show.bind(data), false);
+  document.getElementById('button').addEventListener('click', data, false);
 }, false);
