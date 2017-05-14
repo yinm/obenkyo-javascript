e663827dc91435909d59323bf48f24324ff7f256
yinm <yinm.cminor9@gmail.com>
2017-05-15 08:17:08 +0900

    prototypeで、メソッド定義

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,12 +1,11 @@
 var Member = function(firstName, lastName) {
-  if (!(this instanceof Member)) {
-    return new Member(firstName, lastName);
-  }
   this.firstName = firstName;
   this.lastName  = lastName;
 };
 
-var m = Member('二郎', '鈴木');
-console.log(m);
-// console.log(firstName); // Reference Errorになるため、コメントアウト
-console.log(m.firstName);
+Member.prototype.getName = function() {
+  return this.lastName + ' ' + this.firstName;
+};
+
+var mem = new Member('太郎', '田中');
+console.log(mem.getName());
