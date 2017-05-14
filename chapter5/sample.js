47d3ae930a2cbacfe90fce4fee072aba9fe4c434
yinm <yinm.cminor9@gmail.com>
2017-05-14 11:13:12 +0900

    argumentsオブジェクトを、配列として操作する

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,11 +1,6 @@
-var data = 'Global data';
-var obj1 = {data: 'obj1 data'};
-var obj2 = {data: 'obj2 data'};
-
 function hoge() {
-  console.log(this.data);
+  var args = Array.prototype.slice.call(arguments);
+  console.log(args.join('/'));
 }
 
-hoge.call(null);
-hoge.call(obj1);
-hoge.call(obj2);
+hoge('Angular', 'React', 'Backbone');
