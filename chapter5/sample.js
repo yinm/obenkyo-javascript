eaf1cbc6634d8a350aa84bb1fbc554189ebdb155
yinm <yinm.cminor9@gmail.com>
2017-05-16 08:26:50 +0900

    undefinedを使うことで、インスタンス個別にprototypeプロパティを無効化する

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -4,11 +4,6 @@ Member.prototype.sex = '男';
 
 var mem1 = new Member();
 var mem2 = new Member();
-
 console.log(mem1.sex + '|' + mem2.sex);
-mem2.sex = '女';
-console.log(mem1.sex + '|' + mem2.sex);
-
-delete mem1.sex;
-delete mem2.sex;
+mem2.sex = undefined;
 console.log(mem1.sex + '|' + mem2.sex);
