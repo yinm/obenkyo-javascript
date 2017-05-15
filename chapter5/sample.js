15b9ad501d7c16a35a5b8116def8e9d7235cbe26
yinm <yinm.cminor9@gmail.com>
2017-05-16 08:28:47 +0900

    prototypeプロパティをundefinedした場合でも、プロパティとしては存在することを確認

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,9 +1,9 @@
 var Member = function() {};
 
 Member.prototype.sex = '男';
+var mem = new Member();
+mem.sex = undefined;
 
-var mem1 = new Member();
-var mem2 = new Member();
-console.log(mem1.sex + '|' + mem2.sex);
-mem2.sex = undefined;
-console.log(mem1.sex + '|' + mem2.sex);
+for (var key in mem) {
+  console.log(key + ':' + mem[key]);
+}
