f6a735dfe86f84e15513ba4ebbf8a93d0583cb16
yinm <yinm.cminor9@gmail.com>
2017-05-17 08:26:43 +0900

    constructorプロパティの確認

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,23 +1,9 @@
 var Animal = function() {};
-Animal.prototype = {
-  walk: function() {
-    console.log('トコトコ...');
-  }
-};
+var Hamster = function() {};
+Hamster.prototype = new Animal();
 
-var SuperAnimal = function() {};
-SuperAnimal.prototype = {
-  walk: function() {
-    console.log('ダダダダダ！');
-  }
-};
-
-var Dog = function() {};
-Dog.prototype = new Animal();
-var d1 = new Dog();
-d1.walk();
-
-Dog.prototype = new SuperAnimal();
-var d2 = new Dog();
-d2.walk();
-d1.walk();
+var a = new Animal();
+var h = new Hamster();
+console.log(a.constructor === Animal);
+console.log(h.constructor === Animal);
+console.log(h.constructor === Hamster);
