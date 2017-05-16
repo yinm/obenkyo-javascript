67bcaa63ceafa9fbbdd39953d14e26802353f508
yinm <yinm@pepabo.com>
2017-05-16 09:51:38 +0900

    静的プロパティ・メソッドの定義

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,17 +1,18 @@
-var Member = function(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName  = lastName;
+var Area = function() {};
+
+Area.version = '1.0';
+
+Area.triangle = function(base, height) {
+  return base * height / 2;
 };
 
-Member.prototype = {
-  getName: function() {
-    return this.lastName + ' ' + this.firstName;
-  },
-  toString: function() {
-    return this.lastName + this.firstName;
-  }
+Area.diamond = function(width, height) {
+  return width * height / 2;
 };
 
-var mem1 = new Member('太郎', '田中');
-console.log(mem1.getName());
-console.log(mem1.toString());
+console.log('Areaクラスのバージョン:' + Area.version);
+console.log('三角形の面積:' + Area.triangle(5, 3));
+console.log('菱形の面積:' + Area.diamond(10, 2));
+
+var a = new Area();
+console.log('菱形の面積:' + a.diamond(10, 2));
