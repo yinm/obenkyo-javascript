9b64706c729014a9bf98f1650171570221d215d8
yinm <yinm.cminor9@gmail.com>
2017-05-17 08:11:13 +0900

    プロトタイプチェーンが動的に変化する例 (ただし、インスタンス生成時点で継承は固定される)

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,19 +1,23 @@
 var Animal = function() {};
-
 Animal.prototype = {
   walk: function() {
     console.log('トコトコ...');
   }
 };
 
-var Dog = function() {
-  Animal.call(this);
-};
-Dog.prototype = new Animal();
-Dog.prototype.bark = function() {
-  console.log('ワンワン！');
+var SuperAnimal = function() {};
+SuperAnimal.prototype = {
+  walk: function() {
+    console.log('ダダダダダ！');
+  }
 };
 
-var d = new Dog();
-d.walk();
-d.bark();
+var Dog = function() {};
+Dog.prototype = new Animal();
+var d1 = new Dog();
+d1.walk();
+
+Dog.prototype = new SuperAnimal();
+var d2 = new Dog();
+d2.walk();
+d1.walk();
