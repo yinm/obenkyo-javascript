b1dd2c161992079e65aebcf948325d9cef72523c
yinm <yinm@pepabo.com>
2017-05-18 09:47:44 +0900

    Object.definePropertyメソッド

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -2,32 +2,44 @@ function Triangle() {
   var _base;
   var _height;
 
-  var _checkArgs = function(val) {
-    return (typeof val === 'number' && val > 0);
-  };
+  Object.defineProperty(
+    this,
+    'base',
+    {
+      get: function() {
+        return _base;
+      },
+      set: function(base) {
+        if (typeof base === 'number' && base > 0) {
+          _base = base;
+        }
+      }
+    }
+  );
 
-  this.setBase = function(base) {
-    if (_checkArgs(base)) { _base = base; }
-  };
-  this.getBase = function() { return _base; }
-
-  this.setHeight = function(height) {
-    if (_checkArgs(height)) { _height = height; }
-  };
-  this.getHeight = function() { return _height; }
+  Object.defineProperty(
+    this,
+    'height',
+    {
+      get: function() {
+        return _height;
+      },
+      set: function(height) {
+        if (typeof height === 'number' && height > 0) {
+          _height = height;
+        }
+      }
+    }
+  );
 }
 
 Triangle.prototype.getArea = function() {
-  return this.getBase() * this.getHeight() / 2;
+  return this.base * this.height / 2;
 };
 
 var t = new Triangle();
-t._base = 10;
-t._height = 2;
-console.log('三角形の面積:' + t.getArea());
-
-t.setBase(10);
-t.setHeight(2);
-console.log('三角形の底辺:' + t.getBase());
-console.log('三角形の高さ:' + t.getHeight());
+t.base = 10;
+t.height = 2;
+console.log('三角形の底辺:' + t.base);
+console.log('三角形の高さ:' + t.height);
 console.log('三角形の面積:' + t.getArea());
