088a2c574b848d3d85c6ad755a8a31fbe1eb8380
yinm <yinm.cminor9@gmail.com>
2017-05-20 21:52:54 +0900

    extendsキーワード

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,7 +1,20 @@
-class Area {
-  static getTriangle(base, height) {
-    return base * height / 2;
+class Member {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName  = lastName;
+  }
+
+  getName() {
+    return this.lastName + this.firstName;
   }
 }
 
-console.log(Area.getTriangle(10, 5));
+class BusinessMember extends Member {
+  work() {
+    return this.getName() + 'は働いています。';
+  }
+}
+
+let bm = new BusinessMember('太郎', '田中');
+console.log(bm.getName());
+console.log(bm.work());
