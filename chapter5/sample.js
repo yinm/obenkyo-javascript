b453629c7fc6b8aae212b707043174a8132db99f
yinm <yinm.cminor9@gmail.com>
2017-05-20 21:48:10 +0900

    staticキーワード

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,29 +1,7 @@
-class Member {
-  constructor(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName  = lastName;
-  }
-
-  get firstName() {
-    return this._firstName;
-  }
-
-  set firstName(value) {
-    this._firstName = value;
-  }
-
-  get lastName() {
-    return this._lastName;
-  }
-
-  set lastName(value) {
-    this._lastName = value;
-  }
-
-  getName() {
-    return this.lastName + this.firstName;
+class Area {
+  static getTriangle(base, height) {
+    return base * height / 2;
   }
 }
 
-let m = new Member('太郎', '山田');
-console.log(m.getName());
+console.log(Area.getTriangle(10, 5));
