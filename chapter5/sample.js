861fd923807a5905558678b38de51f5a9a4596f4
yinm <yinm.cminor9@gmail.com>
2017-05-20 21:45:12 +0900

    getter/setter

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -4,10 +4,26 @@ class Member {
     this.lastName  = lastName;
   }
 
+  get firstName() {
+    return this._firstName;
+  }
+
+  set firstName(value) {
+    this._firstName = value;
+  }
+
+  get lastName() {
+    return this._lastName;
+  }
+
+  set lastName(value) {
+    this._lastName = value;
+  }
+
   getName() {
     return this.lastName + this.firstName;
   }
 }
 
-let m = new Member('太郎', '田中');
+let m = new Member('太郎', '山田');
 console.log(m.getName());
