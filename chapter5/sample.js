5405dd997d09b6482626e6a4c38a08d031b5b696
yinm <yinm.cminor9@gmail.com>
2017-05-21 10:39:33 +0900

    プロパティの動的生成

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,5 +1,10 @@
-let name = '山田太郎';
-let birth = new Date(1970, 5, 25);
-let member = {name, birth};
+let i = 0;
+let member = {
+  name: '山田太郎',
+  birth: new Date(1970, 5, 25),
+  ['memo' + ++i]: '正規会員',
+  ['memo' + ++i]: '支部会長',
+  ['memo' + ++i]: '関東'
+};
 
 console.log(member);
