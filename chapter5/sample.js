6bf1d357e29617ac5e54280386941d2b797b298b
yinm <yinm.cminor9@gmail.com>
2017-05-21 16:39:06 +0900

    自作クラスのイテレータ

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,8 +1,22 @@
-let data_array = ['one', 'two', 'three'];
-let itr = data_array[Symbol.iterator]();
-let d;
-while (d = itr.next()) {
-  if (d.done) {break;}
-  console.log(d.done);
-  console.log(d.value);
+class MyIterator {
+  constructor(data) {
+    this.data = data;
+  }
+
+  [Symbol.iterator]() {
+    let current = 0;
+    let that = this;
+    return {
+      next() {
+        return current < that.data.length
+          ? {value: that.data[current++], done: false}
+          : {done: true}
+      }
+    };
+  }
+}
+
+let itr = new MyIterator(['one', 'two', 'three']);
+for (let value of itr) {
+  console.log(value);
 }
