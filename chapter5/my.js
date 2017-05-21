d392006127d9f57931219dc35456b4ef5fbc0363
yinm <yinm.cminor9@gmail.com>
2017-05-21 11:20:16 +0900

    defualt exportを使う

--- chapter5/my.js
+++ chapter5/my.js
@@ -1,4 +1,32 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var _class = function () {
+  function _class() {
+    _classCallCheck(this, _class);
+  }
+
+  _createClass(_class, null, [{
+    key: "getTriangle",
+    value: function getTriangle(base, height) {
+      return base * height / 2;
+    }
+  }]);
+
+  return _class;
+}();
+
+exports.default = _class;
+
+},{}],2:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -49,13 +77,15 @@ var Area = exports.Area = function () {
   return Area;
 }();
 
-},{}],2:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 'use strict';
 
-var _Util = require('./lib/Util');
+var _Area = require('./lib/Area');
 
-var m = new _Util.Member('三郎', '山田');
-console.log(m.getName());
-console.log(_Util.Area.getTriangle(10, 5));
+var _Area2 = _interopRequireDefault(_Area);
 
-},{"./lib/Util":1}]},{},[2,1]);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log(_Area2.default.getTriangle(10, 5));
+
+},{"./lib/Area":1}]},{},[3,2]);
--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,5 +1,2 @@
-import {Member as MyMember, Area as MyArea} from './lib/Util'
-
-let m = new MyMember('三郎', '山田');
-console.log(m.getName());
-console.log(MyArea.getTriangle(10, 5));
+import Area from './lib/Area';
+console.log(Area.getTriangle(10, 5));
