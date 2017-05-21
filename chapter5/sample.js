27facfd5aab1228bdb6d5fcdc99e54899df8e1f3
yinm <yinm.cminor9@gmail.com>
2017-05-21 16:45:22 +0900

    ジェネレータ

--- chapter5/sample.js
+++ chapter5/sample.js
@@ -1,22 +1,9 @@
-class MyIterator {
-  constructor(data) {
-    this.data = data;
-  }
-
-  [Symbol.iterator]() {
-    let current = 0;
-    let that = this;
-    return {
-      next() {
-        return current < that.data.length
-          ? {value: that.data[current++], done: false}
-          : {done: true}
-      }
-    };
-  }
+function* myGenerator() {
+  yield 'あいうえお';
+  yield 'かきくけこ';
+  yield 'さしすせそ';
 }
 
-let itr = new MyIterator(['one', 'two', 'three']);
-for (let value of itr) {
-  console.log(value);
+for (let t of myGenerator()) {
+  console.log(t);
 }
