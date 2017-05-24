5af95aaac271f066d4ff9d7b4156599763b47fd0
yinm <yinm.cminor9@gmail.com>
2017-05-24 21:48:54 +0900

    setInterval

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,7 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('fm').addEventListener('submit', function(e) {
-    if (window.confirm('ページを送信してもいいですか？')) {
-      e.preventDefault();
-    }
+  let timer = window.setInterval(
+    function() {
+      let dat = new Date();
+      document.getElementById('result').textContent = dat.toLocaleTimeString();
+    }, 5000);
+
+  document.getElementById('btn').addEventListener('click', function() {
+    window.clearInterval(timer);
   }, false);
 }, false);
