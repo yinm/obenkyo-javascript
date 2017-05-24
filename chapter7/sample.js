c3a1a2d6db27f6cfa315ffe715d1e8bfd8f149ce
yinm <yinm.cminor9@gmail.com>
2017-05-24 21:58:37 +0900

    setTimeout

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let timer = window.setInterval(
+  let timer = window.setTimeout(
     function() {
       let dat = new Date();
       document.getElementById('result').textContent = dat.toLocaleTimeString();
     }, 5000);
 
   document.getElementById('btn').addEventListener('click', function() {
-    window.clearInterval(timer);
+    window.clearTimeout(timer);
   }, false);
 }, false);
