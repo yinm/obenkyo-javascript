a5d0472532d8716c11570b32e9bbaf2cfc238c9e
yinm <yinm.cminor9@gmail.com>
2017-05-25 08:24:25 +0900

    historyオブジェクト

--- chapter7/sample.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log(location.href);
-
-  document.getElementById('isbn').addEventListener('change', function() {
-    location.replace('http://www.wings.msn.to/index.php/-/A-03/' + this.value);
-  }, false);
-}, false);
