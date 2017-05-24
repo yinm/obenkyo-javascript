1b3fd32eef15d67303b8e435681cc025cc4131e4
yinm <yinm.cminor9@gmail.com>
2017-05-25 08:22:16 +0900

    location.replace

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log(location.href);
 
   document.getElementById('isbn').addEventListener('change', function() {
-    location.href = 'http://www.wings.msn.to/index.php/-/A-03/' + this.value;
+    location.replace('http://www.wings.msn.to/index.php/-/A-03/' + this.value);
   }, false);
 }, false);
