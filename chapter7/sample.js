91ecf21f3d6e918007aa47187aff3348be080d03
yinm <yinm.cminor9@gmail.com>
2017-05-25 23:22:35 +0900

    console.assert

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,3 +1,6 @@
-console.time('MyTimer');
-window.alert('確認してください');
-console.timeEnd('MyTimer');
+function circle(radius) {
+  console.assert(typeof radius === 'number' && radius > 0, '引数radiusは正数でなければいけません。');
+  return radius * radius * Math.PI;
+}
+
+console.log(circle(-5));
