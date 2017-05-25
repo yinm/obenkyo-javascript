9eca26a0316b5bd33d8d2d399d5d656070d8c36a
yinm <yinm.cminor9@gmail.com>
2017-05-25 23:04:08 +0900

    consoleオブジェクトの種類確認

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,14 +1,4 @@
-let agent = window.navigator.userAgent.toLowerCase();
-let version = window.navigator.appVersion.toLowerCase();
-
-let msie = (agent.indexOf('msie') > -1) && (agent.indexOf('opr') === -1);
-let ie10 = msie && (version.indexOf('msie 10.') > -1);
-console.log('Internet Exploer10: ' + ie10);
-
-var chrome =  (agent.indexOf('chrome') > -1)
-           && (agent.indexOf('edge') === -1)
-           && (agent.indexOf('opr')  === -1);
-console.log('Chrome: ' + chrome);
-
-let firefox = (agent.indexOf('firefox') > -1);
-console.log('Firefox: ' + firefox);
+console.log('ログ');
+console.info('情報');
+console.warn('警告');
+console.error('エラー');
