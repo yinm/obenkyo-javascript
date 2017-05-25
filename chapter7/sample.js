7e8d4f58266c69f0037037cbda37e53276298296
yinm <yinm.cminor9@gmail.com>
2017-05-25 23:41:35 +0900

    localStorage

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,5 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let d = document.getElementById('main');
-  console.log(d);
-  console.dir(d);
-}, false);
+let storage = localStorage;
+storage.setItem('fruit1', 'りんご');
+storage.fruit2 = 'みかん';
+storage['fruit3'] = 'ぶどう';
+console.log(storage.getItem('fruit1'));
+console.log(storage.fruit2);
+console.log(storage['fruit3']);
