b814d99045b72b750f351acb85480d3bf89ff737
yinm <yinm@pepabo.com>
2017-05-26 09:46:45 +0900

    オブジェクト変換用のクラス

--- chapter7/sample.js
+++ chapter7/sample.js
@@ -1,5 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-  let d = document.getElementById('main');
-  console.log(d);
-  console.dir(d);
-}, false);
+let MyStorage = function(app) {
+  this.app = app;
+  this.storage = localStorage;
+  this.data = JSON.parse(this.storage[this.app] || '{}');
+};
+
+MyStorage.prototype = {
+  getItem: function(key) {
+    return this.data[key];
+  },
+  setItem: function(key, value) {
+    this.data[key] = value;
+  },
+  save: function() {
+    this.storage[this.app] = JSON.stringify(this.data);
+  }
+};
+
+let storage = new MyStorage('JSSample');
+storage.setItem('hoge', 'ほげ');
+console.log(storage.getItem('hoge'));
+storage.save();
